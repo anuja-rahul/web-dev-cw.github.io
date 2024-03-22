fb5a6b55d94fd09387163715ecd5451eca412a8d
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-03-22 21:07:03 +0530

    added custom curser

--- res/js/main.js
+++ res/js/main.js
@@ -7,3 +7,9 @@ function openmenu(){
 function closemenu(){
     sidemenu.style.right = "-200px";
 }
+
+document.addEventListener('mousemove', function(e) {
+    var cursor = document.querySelector('.custom-cursor');
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+});
