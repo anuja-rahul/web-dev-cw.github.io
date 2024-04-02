36b628361b32e800f6b29c77b346c9c6b2e8cccb
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-04-03 00:33:00 +0530

    fixed menu bug

---
+++ res/js/common.js
@@ -0,0 +1,9 @@
+let sidemenu = document.getElementById("sidemenu");
+
+function openMenu(){
+    sidemenu.style.right = "0";
+};
+
+function closeMenu(){
+    sidemenu.style.right = "-200px";
+};
--- res/js/main.js
+++ res/js/main.js
@@ -1,13 +1,3 @@
-let sidemenu = document.getElementById("sidemenu");
-
-function openMenu(){
-    sidemenu.style.right = "0";
-};
-
-function closeMenu(){
-    sidemenu.style.right = "-200px";
-};
-
 // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_typewriter
 
 const textArray = ["Our mission is to conserve the life below water.", 
