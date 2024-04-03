eed3f29175c094031ac65dcb6ba756a4f3a5ed11
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-04-03 15:25:37 +0530

    page editor finished

---
+++ res/js/editor.js
@@ -0,0 +1,15 @@
+// https://github.com/anuja-rahul/css-animations/tree/main/animate_on_scroll
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
